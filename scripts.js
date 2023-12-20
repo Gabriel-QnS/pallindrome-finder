fb5fef2ada9fa4c856c3ceacde778985175953af
Gabriel R. <gabrielrengifo211@gmail.com>
2023-12-20 13:10:56 -0400

    added filtering function

--- scripts.js
+++ scripts.js
@@ -10,9 +10,19 @@ function palindrome(str) {
   const isLowercaseLetter = (charCode) => (charCode >= 97 && charCode <= 122)? true : false;
   const isNumber = (charCode) => (charCode >= 48 && charCode <= 57)? true : false;
 
+  const cleanser = (str) => {
+    const arr = toArray(str);
+    const cleanArr = arr.filter((char) => {
+      let x = char.charCodeAt(0);
+      if (isNumber(x)) return char;
+      else if (isLowercaseLetter(x)) return char;
+      else;
+    });
+    return cleanArr
+  }
+
   // test area do not change
 
-  console.log(isLetter('a'.charCodeAt(0)))
-  console.log(isLetter('T'.charCodeAt(0)))
-  console.log(isNumber('2'.charCodeAt(0)))
-  console.log(isNumber('y'.charCodeAt(0)))
+console.log(cleanser('123'))
+console.log(cleanser('1 2*3'))
+console.log(cleanser('1_____2|@#|@#|@#*3``ñ´´+´´´++´+´+´+´++'))
