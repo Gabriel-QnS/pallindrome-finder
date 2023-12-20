e76e8bbb1b284233a902a084065b9d0bd24b20f5
Gabriel R. <gabrielrengifo211@gmail.com>
2023-12-20 13:02:15 -0400

    logical detectors added

--- scripts.js
+++ scripts.js
@@ -5,9 +5,14 @@ function palindrome(str) {
   palindrome("eye");
 
   const isString = (str) => (typeof str == 'string')? true : false;
-  
+  const toArray = (str) => Array.from(str);
+
+  const isLowercaseLetter = (charCode) => (charCode >= 97 && charCode <= 122)? true : false;
+  const isNumber = (charCode) => (charCode >= 48 && charCode <= 57)? true : false;
 
   // test area do not change
 
-  console.log(isString('hola'))
-  console.log(isString(123))
+  console.log(isLetter('a'.charCodeAt(0)))
+  console.log(isLetter('T'.charCodeAt(0)))
+  console.log(isNumber('2'.charCodeAt(0)))
+  console.log(isNumber('y'.charCodeAt(0)))
